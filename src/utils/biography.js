e746d940cc4e86b592d4f4ddad86605ec6d1a452
dheena-thayalan <dheenathayalanbscct@gmail.com>
2023-12-26 01:59:58 +0530

    - Biography updated

--- src/utils/biography.js
+++ src/utils/biography.js
@@ -1,154 +1,191 @@
-export const biography ={
-    name:'DheenaThayalan M',
-    email:'xxxx.gmail.com',
-    mobile_no:'1025453525342',
-    about_me:`Developer from India.
-             I'm a techno explorer across different domains. I take great care in the experience, architecture, and code quality of the things I build.
-             I am also an open-source enthusiast. I learned a lot from the open-source community and love to collaborate and share knowledge in open-source projects.
-            ⚡ Ping me about anything, I am happy to help 💬`,
-    education:[
-        {
-            name:'SSLC',
-            percentage:'99%',
-            year:'2010-2015',
-            institute:'XXXXInstitute',
-            url:'https://www.rkrschool.in/management.php',
-            address:'Udumalpet'
-        },
-        {
-            name:'HSC',
-            percentage:'99%',
-            year:'2010-2015',
-            institute:'XXXXInstitute',
-            url:'https://www.rkrschool.in/management.php',
-            address:'Udumalpet'
-        },
-        {
-            name:'BSC',
-            percentage:'99%',
-            year:'2017-2020',
-            institute:'XXXXInstitute',
-            branch:'computertechnology',
-            url:'https://stc.ac.in/',
-            address:'Udumalpet'
-        },
-    ],
-    work:[
-        {
-            name:'ABC-company',
-            description:'One of the fastest growing companies in the industry and proficiency in areas such as mobile and web app design and development.',
-            address:'',
-            role:'Freelancing-developer',
-            technology:['react','angular','nodejs','postman','filezilla','Ant-ui','mui','scss'],
-            year:'07-2020 to 10-2020',
-        },
-        {
-            name:'Ce-company',
-            description:'One of the fastest growing companies in the industry and proficiency in areas such as mobile and web app design and development.',
-            address:'Bangalore, india',
-            role:'Software developer',
-            technology:['react','angular','nodejs','postman','filezilla','Ant-ui','mui','scss'],
-            year:'07-2021 to 01-2023',
-        },
-    ],
-    skills:[
-        // framework:'React, Angular, Express',
-        // server:'nodejs',
-        // ui_libraries:'MUI, ANTD, Bootstrap',
-        // data_base:'MongoDB, SQL',
-        // programming_language:'Javascript, Java',
-        // tools:'VS Code, Postman, filezilla',
-        {name:'React',/*proficiency:'100%' */},
-        {name:'Angular'},
-        {name:'Javascript, Java'},
-        {name:'Redux'},
-        {name:'middleware thunk'},
-        {name:'HTML5 & CSS/SCSS'},
-        {name:'UI Libraries(MUI,ANTD,Bootstrap)'},
-        {name:'RND Tasks'},
-        {name:'nodejs & Express'},
-        {name:'MongoDB & SQL'},
-    ],
-    projects:[
-        {
-            name:'Logistic',
-            description:'Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed',
-            link:'private',
-            status:'on-going',
-            technology:['Angular11,postman,ANTD,Bootstrap','SCSS']
-        },
-        {
-            name:'Logistic',
-            description:'Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed',
-            link:'private',
-            status:'on-going',
-            technology:['Angular11,postman,ANTD,Bootstrap','SCSS']
-        },
-        {
-            name:'Logistic',
-            description:'Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed',
-            link:'private',
-            status:'on-going',
-            technology:['Angular11,postman,ANTD,Bootstrap','SCSS']
-        },
-        {
-            name:'Logistic',
-            description:'Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed',
-            link:'private',
-            status:'on-going',
-            technology:['Angular11,postman,ANTD,Bootstrap','SCSS']
-        }
-    ],
-    social_media:[
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable prettier/prettier */
+
+export const biography = {
+  name: "DheenaThayalan M",
+  s_name: "Dheena",
+  email: "xxxx.gmail.com",
+  mobile_no: "1025453525342",
+  quotes:"For me, coding is the key to turning awesome concepts into reality",
+  about_me: `<div class=profile-container>
+  <article>
+   <span>Hey there! It's Dheena, UI developer from India. Also I do some backend development.</span>
+   <span>I explore diverse tech realms, prioritizing user experience, robust architecture, and top-notch code quality in my creations.</span>
+   <span> I am also an open-source enthusiast. I learned a lot from the open-source community and I love to collaborateand share knowledge in open-source projects.</span>
+   <ul>
+     <li>Ping me about anything, I am happy to help</li>
+     <li>I'm currently deepening my proficiency in Node.js development</li>
+     <li> And I'm mostly active on LinkedIn and Instagram 💬</li>
+   </ul>
+   </article>
+ </div>`,
+  education: [
+    {
+      name: "SSLC",
+      percentage: "99%",
+      year: "2010-2015",
+      institute: "XXXXInstitute",
+      url: "https://www.rkrschool.in/management.php",
+      address: "Udumalpet",
+    },
+    {
+      name: "HSC",
+      percentage: "99%",
+      year: "2010-2015",
+      institute: "XXXXInstitute",
+      url: "https://www.rkrschool.in/management.php",
+      address: "Udumalpet",
+    },
+    {
+      name: "BSC",
+      percentage: "99%",
+      year: "2017-2020",
+      institute: "XXXXInstitute",
+      branch: "computertechnology",
+      url: "https://stc.ac.in/",
+      address: "Udumalpet",
+    },
+  ],
+  work: [
+    {
+      name: "ABC-company",
+      description:
+        "One of the fastest growing companies in the industry and proficiency in areas such as mobile and web app design and development.",
+      address: "",
+      role: "Freelancing-developer",
+      technology: [
+        "react",
+        "angular",
+        "nodejs",
+        "postman",
+        "filezilla",
+        "Ant-ui",
+        "mui",
+        "scss",
+      ],
+      year: "07-2020 to 10-2020",
+    },
+    {
+      name: "Ce-company",
+      description:
+        "One of the fastest growing companies in the industry and proficiency in areas such as mobile and web app design and development.",
+      address: "Bangalore, india",
+      role: "Software developer",
+      technology: [
+        "react",
+        "angular",
+        "nodejs",
+        "postman",
+        "filezilla",
+        "Ant-ui",
+        "mui",
+        "scss",
+      ],
+      year: "07-2021 to 01-2023",
+    },
+  ],
+  skills: [
+    // framework:'React, Angular, Express',
+    // server:'nodejs',
+    // ui_libraries:'MUI, ANTD, Bootstrap',
+    // data_base:'MongoDB, SQL',
+    // programming_language:'Javascript, Java',
+    // tools:'VS Code, Postman, filezilla',
+    { name: "React" /* proficiency:'100%' */ },
+    { name: "Angular" },
+    { name: "Javascript, Java" },
+    { name: "Redux" },
+    { name: "middleware thunk" },
+    { name: "HTML5 & CSS/SCSS" },
+    { name: "UI Libraries(MUI,ANTD,Bootstrap)" },
+    { name: "RND Tasks" },
+    { name: "nodejs & Express" },
+    { name: "MongoDB & SQL" },
+  ],
+  projects: [
+    {
+      name: "Logistic",
+      description:
+        "Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed",
+      link: "private",
+      status: "on-going",
+      technology: ["Angular11,postman,ANTD,Bootstrap", "SCSS"],
+    },
+    {
+      name: "Logistic",
+      description:
+        "Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed",
+      link: "private",
+      status: "on-going",
+      technology: ["Angular11,postman,ANTD,Bootstrap", "SCSS"],
+    },
+    {
+      name: "Logistic",
+      description:
+        "Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed",
+      link: "private",
+      status: "on-going",
+      technology: ["Angular11,postman,ANTD,Bootstrap", "SCSS"],
+    },
+    {
+      name: "Logistic",
+      description:
+        "Cloud-based TMS (Transport Management System)  is a logistics platform that uses technology to help businesses plan, execute and optimize the physical movement of goods, both incoming and outgoing, and making sure the shipment is compliant, proper documentation is managed",
+      link: "private",
+      status: "on-going",
+      technology: ["Angular11,postman,ANTD,Bootstrap", "SCSS"],
+    },
+  ],
+  social_media: [
     // {
     //   href: 'mailto:dheenathayalanbsc2000@gmail.com',
     //   icons: 'mail',
     //   name: 'mail'
     // },
     {
-        href: 'https://www.linkedin.com/in/dheenathayalan-m-8a198a1a3/detail/contact-info/',
-        icons: 'linkedin',
-        name: 'Linkedin'
-      },
-      {
-        href: 'https://www.hackerrank.com/dheenathayalanb2',
-        icons: 'partition',
-        name: 'HackerRank'
-      },
-      {
-        href: 'https://www.instagram.com/_dheena__/',
-        icons: 'instagram',
-        name: 'Instagram'
-      },
-      {
-        href: 'https://github.com/dheena-thayalan',
-        icons: 'github',
-        name: 'GitHub'
-      },
-      {
-        href: 'https://twitter.com/dheenat88556319',
-        icons: 'twitter',
-        name: 'twitter'
-      },
-      {
-        href: 'https://www.facebook.com/vj.dheena/',
-        icons: 'facebook',
-        name: 'Facebook'
-      },
-    ],
-    languageData: [
-        {
-         language: 'English',
-          rate: 4.5
-        },
-        {
-          language: 'Tamil',
-          rate: 5
-        },
-        {
-          language: 'Kannada',
-          rate: 2.5
-        },
-      ]
-
-}
+      href: "https://www.linkedin.com/in/dheenathayalan-m-8a198a1a3/detail/contact-info/",
+      icons: "linkedin",
+      name: "Linkedin",
+    },
+    {
+      href: "https://www.hackerrank.com/dheenathayalanb2",
+      icons: "partition",
+      name: "HackerRank",
+    },
+    {
+      href: "https://www.instagram.com/_dheena__/",
+      icons: "instagram",
+      name: "Instagram",
+    },
+    {
+      href: "https://github.com/dheena-thayalan",
+      icons: "github",
+      name: "GitHub",
+    },
+    {
+      href: "https://twitter.com/dheenat88556319",
+      icons: "twitter",
+      name: "twitter",
+    },
+    {
+      href: "https://www.facebook.com/vj.dheena/",
+      icons: "facebook",
+      name: "Facebook",
+    },
+  ],
+  languageData: [
+    {
+      language: "English",
+      rate: 4.5,
+    },
+    {
+      language: "Tamil",
+      rate: 5,
+    },
+    {
+      language: "Kannada",
+      rate: 2.5,
+    },
+  ],
+};
